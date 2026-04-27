d28aeb4e97f9f20b8588d64bb8a2a31ed8f845f2
rocyon <rocyon@pm.me>
2026-04-26 22:38:27 -0500

    Editable Profile

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -11,13 +11,31 @@ class _ProfilePage extends State<ProfilePage> {
   static const _osuURL =
       'https://go.okstate.edu/go-pokes/images/mobile-images/apple-watch-42-mm/apple-watch-42-9.jpg';
 
+  var editing = true;
+  var name = "Reese Roberts";
+  var title = "Flutter Developer | CS Student";
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('My Profile')),
+      appBar: AppBar(
+        title: const Text('My Profile'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              setState(() {
+                editing = !editing;
+              });
+            },
+            icon: Icon(
+              editing ? Icons.save : Icons.edit,
+            ),
+          ),
+        ],
+      ),
       body: Center(
         child: Column(
-          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
             // Avitar
             const CircleAvatar(
@@ -28,14 +46,46 @@ class _ProfilePage extends State<ProfilePage> {
 
             // Name and title
             const SizedBox(height: 12),
-            const Text(
-              'Reese Roberts',
-              style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold),
+            
+            SizedBox(
+              width: 300,
+              child: editing ? TextField(
+                textAlign: .center,
+                controller: TextEditingController(text: name),
+                onSubmitted: (newValue) {
+                  setState(() {
+                    name = newValue;
+                  });
+                },
+              ) : Text(
+                name,
+                textAlign: .center,
+                style: TextStyle(fontSize: 26, fontWeight: .bold),
+              )
             ),
-            Text(
-              'Flutter Developer | CS Student',
-              style: TextStyle(fontSize: 16, color: Colors.grey.shade600),
+
+            SizedBox(
+              width: 300,
+              child: editing ? TextField(
+                textAlign: .center,
+                controller: TextEditingController(text: title),
+                onSubmitted: (newValue) {
+                  setState(() {
+                    title = newValue;
+                  });
+                },
+              ) : Text(
+                title,
+                textAlign: .center,
+                style: TextStyle(fontSize: 16, color: Colors.grey.shade800),
+              )
             ),
+
+            
+            // Text(
+            //   'Flutter Developer | CS Student',
+            //   style: TextStyle(fontSize: 16, color: Colors.grey.shade600),
+            // ),
 
             // Return Button
             const SizedBox(height: 12),
