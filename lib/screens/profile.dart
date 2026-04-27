47c87ec8c745a7711e81f390d9848833ba5545e4
rocyon <rocyon@pm.me>
2026-04-26 23:01:35 -0500

    Add Contact Page

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+import 'dart:io' show Platform;
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
@@ -7,9 +10,10 @@ class ProfilePage extends StatefulWidget {
   State<ProfilePage> createState() => _ProfilePage();
 }
 
+const _osuURL = 'https://go.okstate.edu/go-pokes/images/mobile-images/apple-watch-42-mm/apple-watch-42-9.jpg';
+
 class _ProfilePage extends State<ProfilePage> {
-  static const _osuURL =
-      'https://go.okstate.edu/go-pokes/images/mobile-images/apple-watch-42-mm/apple-watch-42-9.jpg';
+  static const NetworkImage _profileImage = NetworkImage(_osuURL);
 
   var editing = true;
   var name = "Reese Roberts";
@@ -40,7 +44,7 @@ class _ProfilePage extends State<ProfilePage> {
             // Avitar
             const CircleAvatar(
               radius: 56,
-              backgroundImage: NetworkImage(_osuURL),
+              backgroundImage: _profileImage,
               backgroundColor: Colors.transparent,
             ),
 
@@ -89,14 +93,71 @@ class _ProfilePage extends State<ProfilePage> {
 
             // Return Button
             const SizedBox(height: 12),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              child: const Text('Go Back'),
-            ),
+            Row(
+              mainAxisAlignment: .center,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    Route route = Platform.isIOS
+                        ? CupertinoPageRoute(builder: (_) => ContactPage(name: name))
+                        : MaterialPageRoute(builder: (_) => ContactPage(name: name));
+                    Navigator.push(context, route);
+                  }, 
+                  child: const Text("Contact Info")
+                ),
+
+                const SizedBox(width: 12),
+
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: const Text('Go Back'),
+                )
+              ],
+            )
           ],
         ),
+      ),
+    );
+  }
+}
+
+class ContactPage extends StatelessWidget {
+  const ContactPage({super.key, required this.name});
+  
+  final String name;
+
+  static const contactText = TextStyle(fontSize: 18);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Contact Info'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: .center,
+          children: [
+            const CircleAvatar(
+              radius: 56,
+              backgroundImage: NetworkImage(_osuURL),
+              backgroundColor: Colors.transparent,
+            ),
+
+            // Name and title
+            const SizedBox(height: 12),
+            Text(
+              name,
+              textAlign: .center,
+              style: TextStyle(fontSize: 26, fontWeight: .bold),
+            ),
+
+            const Text("email: example@site.test", style: contactText),
+            const Text("phone: (123) 456-7890", style: contactText),
+          ],
+        )
       ),
     );
   }
